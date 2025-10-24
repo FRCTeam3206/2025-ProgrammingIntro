072357e6cfe73100b85c322f40e62f510916ec05
CREATE-02 <frc-create@outlook.com>
2025-10-23 19:49:25 -0500

    Added Comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,6 +27,10 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     configureBindings();
+    //Hello
+    /*Hello
+     * Hello 2
+     Hello2*/
   }
 
    /**
