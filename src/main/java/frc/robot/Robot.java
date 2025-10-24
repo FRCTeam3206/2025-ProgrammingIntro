f88255411c18e4426e6a7229b431d404905f9f9c
CREATE-04 <frc-create@outlook.com>
2025-10-23 19:44:16 -0500

    added a comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,6 +52,8 @@ public class Robot extends TimedRobot {
     return new RunCommand(() -> {});
   }
 
+//ignore line 55
+
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
